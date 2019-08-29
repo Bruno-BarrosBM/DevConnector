c6eaed9f9f4e55521aee7ef8eef33cbef5aeef7f
Bruno-BarrosBM <bruno_abarros@hotmail.com>
2019-08-29 13:42:37 -0300

    Logout & Navbar links

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,18 +4,19 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_SUCCESS,
-    LOGIN_FAIL
-  } from '../actions/types';
+    LOGIN_FAIL,
+    LOGOUT
+  } from '../actions/types'
   
   const initialState = {
     token: localStorage.getItem('token'),
     isAuthenticated: null,
     loading: true,
     user: null
-  };
+  }
   
   export default function(state = initialState, action) {
-    const { type, payload } = action;
+    const { type, payload } = action
   
     switch (type) {
       case USER_LOADED:
@@ -24,28 +25,28 @@ import {
           isAuthenticated: true,
           loading: false,
           user: payload
-        };
+        }
       case REGISTER_SUCCESS:
       case LOGIN_SUCCESS:
-        localStorage.setItem('token', payload.token);
+        localStorage.setItem('token', payload.token)
         return {
           ...state,
           ...payload,
           isAuthenticated: true,
           loading: false
-        };
+        }
       case REGISTER_FAIL:
       case AUTH_ERROR:
       case LOGIN_FAIL:
-        console.log(123);
-        localStorage.removeItem('token');
+      case LOGOUT:
+        localStorage.removeItem('token')
         return {
           ...state,
           token: null,
           isAuthenticated: false,
           loading: false
-        };
+        }
       default:
-        return state;
+        return state
     }
   }
