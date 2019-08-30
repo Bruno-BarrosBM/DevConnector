b015e843e053da59392c9a8c3f178465694b8de3
Bruno-BarrosBM <bruno_abarros@hotmail.com>
2019-08-30 11:29:18 -0300

    Create profile component & profile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -18,7 +18,42 @@ export const getCurrentProfile = () => async dispatch => {
     } catch (err) {
         dispatch({
             type: PROFILE_ERROR,
-            payload: { msg: err.response.statusText, status: err.response.status}
+            payload: { msg: err.response.statusText, status: err.response.status }
         })
+    }
+}
+
+// Create or update profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const res = await axios.post('/api/profile', formData, config)
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        })
+
+        dispatch(setAlert(edit ? 'Profile updated' : 'Profile Created', 'sucess'))
+
+        if (!edit) {
+            history.push('/dashboard')
+        }
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+
+        const errors = err.response.data.errors
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+        }
     }
 }
